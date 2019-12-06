8a595349aa932a2a4d498d413282ef7c590e1b52
zhuqingshuang <qingshuang_zhu@163.com>
2019-12-07 02:37:40 +0800

    implement stack

---
+++ stack/stack.cpp
@@ -0,0 +1,205 @@
+/*
+ * stack
+ */
+
+#include "stack.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/* 初始化 stack */
+int initStack(sqStack *s) {
+    s->bottom = (dataType*)malloc(STACK_INIT_SIZE*sizeof(dataType));
+    if(NULL == s->bottom)
+    {
+        return 0;
+    }
+    s->top = s->bottom;
+    s->size = STACK_INIT_SIZE;
+    return 1;
+}
+
+/* 销毁 stack */
+int destroyStack(sqStack *s) {
+    if (NULL != s->bottom){
+        free(s->bottom);
+        s->bottom = s->top = NULL;
+    }
+    s->size = 0;
+    return 1;
+}
+
+/* 清空 stack */
+int clearStack(sqStack *s) {
+    if(NULL != s->bottom) {
+        memset(s->bottom, 0, s->size * sizeof(dataType));
+    }
+    s->top = s->bottom;
+    return 1;
+}
+
+/* stack 是否为空 */
+int stackEmpty(sqStack *s) {
+    if(s->top == s->bottom) {
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+/* stack的高度 */
+int stackHeight(sqStack *s) {
+    return s->top - s->bottom;
+}
+
+/* 获取栈顶元素 */
+int top(sqStack *s, dataType *data) {
+    if( s->top == s->bottom){
+        return 0;
+    }
+    *data = *(s->top - 1);
+    return 1; 
+}
+
+/* 入栈 */
+int push(sqStack *s, dataType data) {
+    if(stackHeight(s) >= s->size){  /* 栈满 */
+        s->bottom = (dataType*)realloc(s->bottom, (s->size+STACK_INCREMENT)*sizeof(dataType));
+        if(NULL == s->bottom){
+            return 0;
+        }
+        s->top = s->bottom + s->size;
+        s->size += STACK_INCREMENT;
+    }
+    *s->top = data;
+    s->top++;
+    return 1;
+}
+
+/* 出栈 */
+int pop(sqStack *s, dataType *data) {
+    if(s->top == s->bottom){
+        return 0;
+    }
+    *data = *(--s->top);
+    return 1;
+}
+
+/* 遍历栈 */
+void stackTraverse(sqStack *s) {
+    if(stackEmpty(s)){
+        return;
+    }
+    int index = 0;
+    
+    while((s->top - index) != s->bottom){
+        printf("%d\n", *(s->top - index -1));
+        index++;
+    }
+}
+
+
+int initStack2(linkedStack **s) {
+    *s = (linkedStack*)malloc(sizeof(linkedStack));
+    if(NULL == *s){
+        return 0;
+    }
+    (*s)->top = (*s)->bottom = NULL;
+    (*s)->height = 0;
+    return 1;
+}
+
+int destroyStack2(linkedStack **s) {
+    if((*s)->top != (*s)->bottom){
+        stackNode *tmp = (*s)->top;
+        (*s)->top = (*s)->top->next;
+        free(tmp);
+        tmp = NULL;
+    }
+    free(*s);
+    *s = NULL;
+    return 1;
+}
+
+int clearStack2(linkedStack *s) {
+    if(s->top != s->bottom){
+        stackNode *tmp = s->top;
+        s->top = s->top->next;
+        free(tmp);
+        tmp = NULL;
+    }
+    s->top = s->bottom = NULL;
+    s->height = 0;
+    return 1;
+}
+
+int stackEmpty2(linkedStack *s) {
+    if(NULL == s->top && NULL == s->bottom){
+        return 1;
+    }
+    return 0;
+}
+
+int stackHeight2(linkedStack *s){
+    return s->height;
+}
+
+int top2(linkedStack *s, dataType *data) {
+    if(stackEmpty2(s)){
+        return 0;
+    }
+    *data = s->top->data;
+    return 1;
+}
+
+int push2(linkedStack *s, dataType data) {
+    stackNode *tmp = (stackNode*)malloc(sizeof(stackNode));
+    if(NULL == tmp){
+        return 0;
+    }
+    if(stackEmpty2(s)){
+        tmp->data = data;
+        s->bottom = s->top = tmp;    /* 空栈栈底和栈顶指向第一个元素 */
+        tmp->next = NULL;           /* 第一个元素的指向空 */
+        s->height++;
+        return 1;
+    }
+
+    tmp->data = data;
+    tmp->next = s->top;
+    s->top = tmp;
+    s->height++;
+    return 1;
+}
+
+int pop2(linkedStack *s, dataType *data) {
+    if(stackEmpty2(s)) {
+        return 0;
+    }
+
+    if(s->top == s->bottom){    /* 最后一个元素 */
+        *data = s->top->data;
+        free(s->top);
+        s->bottom = s->top = NULL;
+        s->height = 0;
+    }
+
+    stackNode *tmp = s->top;
+    *data = tmp->data;
+    s->top = s->top->next;
+    s->height--;
+    free(tmp);
+    tmp = NULL;
+    return 1;
+}
+
+void stackTraverse2(linkedStack *s) {
+    if(stackEmpty2(s)){
+        return;
+    }
+    stackNode *top = s->top;
+    while(NULL != top){
+        printf("%d\n", top->data);
+        top = top->next;
+    }
+}
---
+++ stack/stack.h
@@ -0,0 +1,57 @@
+/*
+ * stack
+ */
+
+#ifndef STACK_H
+#define STACK_H
+
+#define STACK_INIT_SIZE 100    /* 栈初始大小 */
+#define STACK_INCREMENT 10    /* 分配增量 */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef int dataType;
+
+/* sequential storage structure */
+typedef struct {
+    dataType *top;
+    dataType *bottom;
+    int size;   /* 当前分配的空间大小 */
+} sqStack;
+int initStack(sqStack *s);
+int destroyStack(sqStack *s);
+int clearStack(sqStack *s);
+int stackEmpty(sqStack *s);
+int stackHeight(sqStack *s);
+int top(sqStack *s, dataType *data);
+int push(sqStack *s, dataType data);
+int pop(sqStack *s, dataType *data);
+void stackTraverse(sqStack *s);
+
+/* linked storage structure */
+typedef struct sNode {
+    dataType data;
+    struct sNode *next;
+} stackNode;
+typedef struct {
+    stackNode *top;
+    stackNode *bottom;
+    int height;   /* 栈的高度 */
+} linkedStack;
+int initStack2(linkedStack **s);
+int destroyStack2(linkedStack **s);
+int clearStack2(linkedStack *s);
+int stackEmpty2(linkedStack *s);
+int stackHeight2(linkedStack *s);
+int top2(linkedStack *s, dataType *data);
+int push2(linkedStack *s, dataType data);
+int pop2(linkedStack *s, dataType *data);
+void stackTraverse2(linkedStack *s);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
