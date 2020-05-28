643f09d9d41fe1e89abb530993c79668192fa15e
zhuqingshuang <qingshuang_zhu@163.com>
2020-05-29 04:33:15 +0800

    implement KMP algorithm

--- string/string.cpp
+++ string/string.cpp
@@ -2,6 +2,9 @@
  * pattern matching
  */
 
+#include<stdlib.h>
+
+/* brute force */
 int bruteForce1(char *P, int m, char *T, int n){
     int i = 0;
     int j = 0;
@@ -36,4 +39,49 @@ int bruteForce2(char *P, int m, char *T, int n){
             break;
     }
     return i;
+}
+
+/* KMP */
+int* next(char *P, int m){
+    int j = 0;                   /* "主"串指针 */
+    int *N = (int*)malloc(m);    /* next表 */
+    int t = N[0] = -1;           /* 模式串指针（P[-1]通配符）*/
+
+    while(j < m-1){
+        /* 匹配 */
+        if(0 > t || P[j] == P[t]){
+            N[++j] = ++t;
+ 
+            /* 改进 */
+            /* j++;
+             * t++;
+             * N[j] = P[j] != P[t] ? t : N[t];
+             */
+        }
+        /* 失配 */
+        else{
+            t = N[t];
+        }
+    }
+    return N;
+}
+
+int KMP(char *P, int m, char *T, int n){
+    int *next = next(P, m);    /* 构造next表 */
+    int i = 0;                 /* 文本串指针 */
+    int j = 0;                 /* 模式串指针 */
+
+    while(j < m && i < n){
+        if(0 > j || T[i] == P[j]){
+            i++;
+            j++;
+        }
+        /* 否则，P右移，T不回退 */
+        else{
+            j = next[j];
+        }
+    }
+
+    free(next);
+    return i-j;
 }
